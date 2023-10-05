a984ee33fb4a04a8812dafb6e30623e827ec7271
yassminezariat <61391268+yassminezariat@users.noreply.github.com>
2023-10-05 11:25:51 +0100

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
+// comment 1 
 @Entity
 @Table(name = "T_USER")
 public class User implements Serializable {
